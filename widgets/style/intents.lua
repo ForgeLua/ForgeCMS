402122611b88ea6c739dd242c59a440299dea01a
alexis-piquet <alexis.piquet.37@gmail.com>
2024-05-13 12:31:25 +0200

    Declare locals for intents

--- widgets/style/intents.lua
+++ widgets/style/intents.lua
@@ -1,25 +1,35 @@
 local CommonUtils = require('common.utils')
 
+local primary = '#b39475'
+local secondary = '#abacb0'
+local success = '#27ae60'
+local danger = '#9f1600'
+local warning = '#b06601'
+local info = '#3498db'
+local dark = '#2c3e50'
+local light = '#2c3e50'
+local link = '#2c3e50'
+
 local default_intents = {
-  ['$primary'] = '#b39475',
-  ['$secondary'] = '#abacb0',
-  ['$success'] = '#27ae60',
-  ['$danger'] = '#9f1600',
-  ['$warning'] = '#b06601',
-  ['$info'] = '#3498db',
-  ['$dark'] = '#2c3e50',
-  ['$light'] = '#2c3e50',
-  ['$link'] = '#2c3e50',
+  ['$primary'] = primary,
+  ['$secondary'] = secondary,
+  ['$success'] = success,
+  ['$danger'] = danger,
+  ['$warning'] = warning,
+  ['$info'] = info,
+  ['$dark'] = dark,
+  ['$light'] = light,
+  ['$link'] = link,
   ['$none'] = 'transparent',
-  ['$primary_light'] = CommonUtils.HexToRgba('#b39475', '.6'),
-  ['$secondary_light'] = CommonUtils.HexToRgba('#abacb0', '.6'),
-  ['$success_light'] = CommonUtils.HexToRgba('#27ae60', '.6'),
-  ['$danger_light'] = CommonUtils.HexToRgba('#9f1600', '.6'),
-  ['$warning_light'] = CommonUtils.HexToRgba('#b06601', '.6'),
-  ['$info_light'] = CommonUtils.HexToRgba('#3498db', '.6'),
-  ['$dark_light'] = CommonUtils.HexToRgba('#2c3e50', '.6'),
-  ['$light_light'] = CommonUtils.HexToRgba('#2c3e50', '.6'),
-  ['$link_light'] = CommonUtils.HexToRgba('#2c3e50', '.6'),
+  ['$primary_light'] = CommonUtils.HexToRgba(primary, '.6'),
+  ['$secondary_light'] = CommonUtils.HexToRgba(secondary, '.6'),
+  ['$success_light'] = CommonUtils.HexToRgba(success, '.6'),
+  ['$danger_light'] = CommonUtils.HexToRgba(danger, '.6'),
+  ['$warning_light'] = CommonUtils.HexToRgba(warning, '.6'),
+  ['$info_light'] = CommonUtils.HexToRgba(info, '.6'),
+  ['$dark_light'] = CommonUtils.HexToRgba(dark, '.6'),
+  ['$light_light'] = CommonUtils.HexToRgba(light, '.6'),
+  ['$link_light'] = CommonUtils.HexToRgba(link, '.6'),
 }
 
 function GetIntents()
