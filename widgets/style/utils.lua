8c1b11c4b9aad627dcb04a3e5fabd2b4f955ef0c
alexis-piquet <alexis.piquet.37@gmail.com>
2024-05-27 11:08:20 +0200

    add intents usage and remove old variables

--- widgets/style/utils.lua
+++ widgets/style/utils.lua
@@ -23,16 +23,9 @@ function utils.compile_scss_with_variables()
   end
   
   local modifedScss = string.format([[
-    $color_1: #ebdec2;
-    $color_2: #fff;
-    $color_3: #f7b10a;
-    $color_4: #ffd36b;
-    $color_5: white;
-    $color_6: #5792d5;
     $font-family_1: Open Sans, Arial, Helvetica, sans-serif;
     $font-family_2: Semplicita Pro,Open Sans,Arial,Helvetica,sans-serif;
     $font-family_3: opensans,sans-serif;
-    $transparent: transparent;
     
     // INTENTS
     %s
